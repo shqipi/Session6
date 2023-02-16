396d76dd9c621d017e53b6aa0a116f5a1bc0d8fd
Shqiperim Ramadani <shqipi_r@hotmail.com>
2023-02-16 23:47:41 +0100

    Initial Commit

---
+++ Session6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Session6
+//
+//  Created by shqiperimramadani on 16.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
